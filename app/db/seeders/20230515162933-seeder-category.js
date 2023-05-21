d0acca1ad97a46c498c00bf99971306b3c7ca68f
Benyamn Netanyahu Immanuel Partogi Siregar <benzoldyck@gmail.com>
2023-05-21 21:33:07 +0700

    first commit

---
+++ app/db/seeders/20230515162933-seeder-category.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Categories',
+      [
+        {
+          name: 'Business & Economics',
+          user: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Art & Design',
+          user: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Agriculture',
+          user: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Categories', null, {});
+  },
+};
